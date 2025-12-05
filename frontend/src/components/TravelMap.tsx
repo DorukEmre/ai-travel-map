36d2aab8f99bd64dd349282982367cd11b6ba1e0
Doruk Emre <doruk.emre@outlook.com>
2025-12-05 23:06:48 +0100

    Implement displaying markers on map for each recommended city

--- frontend/src/components/TravelMap.tsx
+++ frontend/src/components/TravelMap.tsx
@@ -2,16 +2,18 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 import type { MarkerInfo } from '@/types/types';
 import { useEffect, useState } from 'react';
+import type { LatLngExpression } from 'leaflet';
 
 
-const TravelMap = () => {
+const TravelMap = ({ cities }: { cities: MarkerInfo[] }) => {
   const [mapHeight, setMapHeight] = useState<number>(64);
+  const [mapCenter, setMapCenter] = useState<LatLngExpression>();
 
-  let markers: MarkerInfo[] = [
-    { position: [40.417, -3.704], popupText: "Madrid, Spain" },
-    { position: [41.3851, 2.1734], popupText: "Barcelona, Spain" },
-    { position: [48.8566, 2.3522], popupText: "Paris, France" },
-  ];
+  // let markers: MarkerInfo[] = [
+  //   { position: [40.417, -3.704], popupText: "Madrid, Spain" },
+  //   { position: [41.3851, 2.1734], popupText: "Barcelona, Spain" },
+  //   { position: [48.8566, 2.3522], popupText: "Paris, France" },
+  // ];
 
   const updateMapHeight = () => {
     const screenWidth = window.innerWidth;
@@ -33,28 +35,41 @@ const TravelMap = () => {
     };
   }, []);
 
+  useEffect(() => {
+    console.log("cities length: ", cities.length)
+    console.log("cities: ", cities)
+    if (cities.length >= 1) {
+      console.log(cities[0].position)
+      setMapCenter(cities[0].position);
+    }
+
+  }, [cities]);
+
   return (
-    <div className={`w-full h-${mapHeight} my-4 rounded-lg overflow-hidden`}>
+    <>
+      {mapHeight && mapCenter &&
+        <div className={`w-full h-${mapHeight} my-4 rounded-lg overflow-hidden`}>
 
-      <MapContainer center={[40.417, -3.704]} zoom={4} scrollWheelZoom={true} style={{ height: "100%", minHeight: "100%" }}>
+          <MapContainer center={mapCenter} zoom={1} scrollWheelZoom={true} style={{ height: "100%", minHeight: "100%" }}>
 
-        <TileLayer
-          attribution='<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+            <TileLayer
+              attribution='<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
 
-        {markers.map((marker, index) => (
-          <Marker key={index} position={marker.position}>
-            <Popup>
-              {marker.popupText}
-            </Popup>
-          </Marker>
-        ))}
+            {cities.map((marker, index) => (
+              <Marker key={index} position={marker.position}>
+                <Popup>
+                  <strong>{marker.city}</strong>: {marker.popupText}
+                </Popup>
+              </Marker>
+            ))}
 
-      </MapContainer>
+          </MapContainer>
 
-    </div>
-
+        </div>
+      }
+    </>
   );
 };
 
